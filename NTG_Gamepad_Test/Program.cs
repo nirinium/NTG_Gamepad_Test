e5bf95023ff0a63ad22d7604285cad57965063c9
Nirinium <wills.colton@gmail.com>
2018-07-04 00:24:20 -0400

    Add project files.

---
+++ NTG_Gamepad_Test/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using SharpDX.XInput;
+using System.Threading;
+
+namespace NTG_Gamepad_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start XGamepadApp");
+            // Initialize XInput
+            var controllers = new[] { new Controller(UserIndex.One), new Controller(UserIndex.Two), new Controller(UserIndex.Three), new Controller(UserIndex.Four) };
+
+            // Get 1st controller available
+            Controller controller = null;
+            foreach (var selectControler in controllers)
+            {
+                if (selectControler.IsConnected)
+                {
+                    controller = selectControler;
+                    break;
+                }
+            }
+
+            if (controller == null)
+            {
+                Console.WriteLine("No XInput controller installed");
+            }
+            else
+            {
+
+                Console.WriteLine("Found a XInput controller available");
+                Console.WriteLine("Press buttons on the controller to display events or escape key to exit... ");
+
+                // Poll events from joystick
+                var previousState = controller.GetState();
+                while (controller.IsConnected)
+                {
+                    if (IsKeyPressed(ConsoleKey.Escape))
+                    {
+                        break;
+                    }
+                    var state = controller.GetState();
+                    if (previousState.PacketNumber != state.PacketNumber)
+                        Console.WriteLine(state.Gamepad);
+                    Thread.Sleep(10);
+                    previousState = state;
+                }
+            }
+            Console.WriteLine("End XGamepadApp");
+        }
+
+        /// <summary>
+        /// Determines whether the specified key is pressed.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified key is pressed; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsKeyPressed(ConsoleKey key)
+        {
+            return Console.KeyAvailable && Console.ReadKey(true).Key == key;
+        }
+    }
+    
+}
